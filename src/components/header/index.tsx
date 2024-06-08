d65e30460d75c22e11b389b82547bf062810694c
Mariia Petrenko <filisinia@gmail.com>
2024-06-08 21:29:01 +0300

    feat: create a component for the heart button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,6 +4,7 @@ import { getUserDataFromLS } from '@/utils/localStorage';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import Heart from '../heart';
 
 const Header = (): JSX.Element => {
   const pathname = usePathname();
@@ -34,20 +35,7 @@ const Header = (): JSX.Element => {
         </div>
         <div className="flex items-center gap-4">
           <Link href="/liked">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="white"
-              className={`size-7 hover:fill-white ${pathname === '/liked' && 'underline underline-offset-4'}`}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-              />
-            </svg>
+            <Heart />
           </Link>
           <Link
             href={userData ? '/user-profile' : '/create-profile'}
