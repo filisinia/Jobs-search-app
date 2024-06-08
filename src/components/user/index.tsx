2bbd3c9a242517e816e429873718fe7d039c2d3f
Mariia Petrenko <113894148+filisinia@users.noreply.github.com>
2024-06-08 16:41:24 +0200

    Fix merge conflicts (#4)
    * feat: create signup form
    
    * feat: create user-profile page
    
    * feat: implement navigation upon logout and signup
    
    * fix: underline profile link

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -15,8 +15,8 @@ const UserInfo = (): JSX.Element => {
 
   return (
     <section className="flex flex-col justify-center gap-4 p-10 max-w-2xl rounded-lg bg-slate-50/5 shadow-lg shadow-indigo-300/10 m-auto mt-10">
-      <div className="self-center w-24 h-24 bg-purple-900 rounded-full flex items-center justify-center">
-        You
+      <div className="self-center w-24 h-24 bg-purple-900 text-2xl rounded-full flex items-center justify-center">
+        {userData.name[0]}
       </div>
       {userElems}
     </section>
